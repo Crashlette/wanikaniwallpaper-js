7de7f30205e07e4f62d946360a5532e1e15f9d47
Crashlette <ashley.e.kenn@gmail.com>
2017-07-30 13:04:40 -0400

    Add files via upload
    first working version. only API key support for wallpaper engine user properties.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,42 @@
 require(["api", "canvasdrawer", "settings"], function(api, canvasDrawer, settings) {
-	var form = document.getElementById("api-form");
+    var form = document.getElementById("api-form");
+    var messageElem = document.getElementById('settingLbl');
+    var messageElem2 = document.getElementById("message4")
 
 	function makeRequest() {
-		var messageElem = document.getElementById('message');
 		messageElem.innerHTML = "Loading...";
 		api.load(function() {
 
 			canvasDrawer.draw(api.characters);
 
 		});
+
 		return false;
 	}
-
-	form.onsubmit = makeRequest;
 	
-	if (settings.api_key) {
-		makeRequest();
-	}
+    //debug for tracking refresh cycles.
+	//var i = 0;
+
+
+    //give the page a little bit of time to load before sending off request
+	setTimeout(function () {
+	    makeRequest();
+	}, 250);
+
+    // submits form every X minutes.Makes interval timer that takes a given refresh time (in minutes) and converts to miliseconds.
+	var refreshTime = 1;
+	setInterval(function () {
+
+        //messageElem2.innerHTML="Refresh cycle #" + i + " with API key: " + settings.api_key;
+        makeRequest();
+       // i += 1;
+    }, refreshTime * 60000);
+	
+    if (settings.api_key) {
+        _.debounce(function () { makeRequest(); }, 1000);
+    }
+    
 	
 });
+
+
---
+++ js/properties.js
@@ -0,0 +1,17 @@
+﻿window.wallpaperPropertyListener = {
+    applyUserProperties: function (properties) {
+        console.log("Loading with key: "+properties.public_API_Key.value);
+        require(["settings"], function (settings) {
+            
+             settings.api_key = properties.public_API_Key.value;
+
+            if (properties.public_API_Key) {
+                _.debounce(function () {
+                    settings.api_key = properties.public_API_Key.value;
+                    console.log("GOGOGO");
+                }, 1000);
+            }
+
+        });
+    }
+};
